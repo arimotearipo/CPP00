671bf693ee915d5de6ea78ac7e8013b83f7177cd
Wan nor adzahari Wan tajuddin <wwan-taj@u91z05s01.42kl.edu.my>
2022-07-30 02:36:09 +0800

    just created basic class functions for Account.cpp

---
+++ ex02/Account.cpp
@@ -0,0 +1,34 @@
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts(void)
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (Account::_totalAmount);
+}
+
+int Account::getNbDeposits(void)
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (Account::_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos(void)
+{
+	
+}
